c330efbf79c6a0c945ade307fdcda442926eb5df
Uzma Nareen <nareen.khan98@gmail.com>
2021-09-29 06:56:50 +0530

    first shopping cart commit

---
+++ src/components/products.js
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    id: 1,
+    name: "Potato",
+    category: "Vegetables",
+    price: "30",
+    img: "https://th.bing.com/th/id/OIP.5VPPPf89lIaKKZa6YmjgqwHaFj?pid=ImgDet&w=700&h=525&rs=1",
+    vendor:"Himachal Pvt Ltd",
+    available: 1,
+  },
+  {
+    id: 2,
+    name: "Banana",
+    description:"Fully Freshed",
+    category: "Fruits",
+    price: "50",
+    img: "https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1024-80.jpg.webp",
+    vendor:"Organic Farms",
+    available: 1,
+  },
+  {
+    id: 3,
+    name: "Drumsticks",
+    description:"Fully Freshed",
+    category: "Vegetables",
+    price: "20",
+    img: "https://www.pngpix.com/wp-content/uploads/2016/03/Drumstick-Cut-PNG-Image.png",
+    vendor:"Mallikarjuna Farms",
+    available: 0,
+  },
+  {
+    id: 4,
+    name: "Orange",
+    description:"Fully Freshed",
+    category: "Vegetables",
+    price: "25",
+    img: "https://th.bing.com/th/id/OIP.M1W8f-P4vkjrv7i1VDgM_wHaE8?pid=ImgDet&rs=1",
+    vendor:"Fruits",
+    available: 1,
+  },
+  
+];
